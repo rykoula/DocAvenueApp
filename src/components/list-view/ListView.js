023c98e0a0030cd7df4818f2a2cad4a3bb43e8ef
rykoula <eric.maung@efrei.net>
2019-02-03 15:32:07 +0100

    :lipstick: Animation collapse expanding and export style component

--- src/components/list-view/ListView.js
+++ src/components/list-view/ListView.js
@@ -1,28 +1,42 @@
 import React, { Component } from "react";
 import {
   TouchableOpacity,
+  TouchableHighlight,
+  Image,
   FlatList,
   View,
   Text,
-  StyleSheet
+  Animated
 } from "react-native";
 import { fetchingPosts } from "../../actions/posts";
-import fetch from "../../actions/posts";
 import { connect } from "react-redux";
+import styles from "./style";
+// import config from "../../config/config.dist";
 import { PAGINATION_MAX } from "../../config/constances";
 
 class ListView extends Component {
   state = {
-    pageSize: 10
+    pageSize: 10,
+    expanded: true,
+    animation: new Animated.Value()
   };
+
+  //icons to expand details for each title
+  icons = {
+    up: require("../../assets/up-arrow.png"),
+    down: require("../../assets/down-arrow.png")
+  };
+
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
+    //Fetching post asynchrononously
     this._fetchPosts();
   }
 
+  //calling action fetchingPosts
   _fetchPosts = () => {
     try {
       this.props.fetchingPosts();
@@ -30,35 +44,113 @@ class ListView extends Component {
       console.log(error);
     }
   };
+
+  _paginationMaxReached = () => {
+    return this.state.pageSize < PAGINATION_MAX;
+  };
+
+  //called by loadMoreBtn to manage pagination
   _loadMoreData() {
-    this.state.pageSize < PAGINATION_MAX &&
+    this._paginationMaxReached() &&
       this.setState({
         pageSize: this.state.pageSize + 10
       });
   }
+
+  //Toggle for animation, to set the initial and final value for expanding and start animation
+  _toggle() {
+    let initialValue = this.state.expanded
+      ? this.state.maxHeight + this.state.minHeight
+      : this.state.minHeight;
+    let finalValue = this.state.expanded
+      ? this.state.minHeight
+      : this.state.maxHeight + this.state.minHeight;
+
+    this.setState({
+      expanded: !this.state.expanded
+    });
+
+    this.state.animation.setValue(initialValue);
+    Animated.spring(this.state.animation, {
+      toValue: finalValue
+    }).start();
+  }
+
+  //Event listener to set the max height of the view for animation
+  _setMaxHeight(event) {
+    this.setState({
+      maxHeight: event.nativeEvent.layout.height
+    });
+  }
+
+  //Event listener to set the min height of the view for animation
+  _setMinHeight(event) {
+    this.setState({
+      minHeight: event.nativeEvent.layout.height
+    });
+  }
+
+  //Rendering footer to set Button at the bottom of the FlatList
   _renderFooter() {
     return (
       <View>
         <TouchableOpacity
           activeOpacity={0.9}
           onPress={this._loadMoreData.bind(this)}
-          style={styles.loadMoreBtn}
+          disabled={this._paginationMaxReached() ? false : true}
+          style={
+            this._paginationMaxReached()
+              ? styles.loadMoreBtn
+              : styles.noLoadMoreBtn
+          }
         >
-          <Text>Load More</Text>
+          <Text style={styles.loadMoreTxt}>Load More</Text>
         </TouchableOpacity>
       </View>
     );
   }
 
+  // <Text style={styles.h1text}>POSTS FETCHED FROM</Text>
+  // <Text style={styles.h2text}>{config.api_url}</Text>
   render() {
-    console.log(this.props);
+    let icon = this.icons["down"];
+    if (this.state.expanded) {
+      icon = this.icons["up"];
+    }
     return (
       <FlatList
         data={this.props.posts.listPosts.slice(0, this.state.pageSize)}
         renderItem={({ item }) => (
-          <View style={styles.flatview}>
-            <Text style={styles.title}>{item.title}</Text>
-          </View>
+          <Animated.View
+            style={[styles.flatview, { height: this.state.animation }]}
+          >
+            <View
+              style={styles.flatview1}
+              onLayout={this._setMinHeight.bind(this)}
+            >
+              <Text numberOfLines={1} style={styles.title}>
+                {item.title}
+              </Text>
+              <TouchableHighlight
+                style={styles.buttonArrow}
+                onPress={this._toggle.bind(this)}
+              >
+                <Image style={styles.buttonImage} source={icon} />
+              </TouchableHighlight>
+            </View>
+            <View onLayout={this._setMaxHeight.bind(this)}>
+              <Text>
+                <Text style={styles.subTitle}>Id: </Text>
+                <Text style={styles.details}>{item.id}</Text>
+              </Text>
+              <Text>
+                <Text style={styles.subTitle}>Title: </Text>
+                <Text style={styles.details}>{item.title}</Text>
+              </Text>
+              <Text style={styles.subTitle}>Body: </Text>
+              <Text style={styles.details}>{item.body}</Text>
+            </View>
+          </Animated.View>
         )}
         keyExtractor={item => item.id.toString()}
         ListFooterComponent={this._renderFooter.bind(this)}
@@ -67,32 +159,7 @@ class ListView extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  flatview: {
-    justifyContent: "center",
-    paddingTop: 10,
-    borderRadius: 2
-  },
-  identifier: {
-    fontSize: 18
-  },
-  title: {
-    fontFamily: "Verdana",
-    fontSize: 20
-  },
-  body: {
-    color: "blue"
-  },
-  loadMoreBtn: {
-    padding: 10,
-    backgroundColor: "#800000",
-    borderRadius: 4,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
-
+//REDUX: Mapping state to props
 const mapStateToProps = state => {
   return {
     posts: state.posts
